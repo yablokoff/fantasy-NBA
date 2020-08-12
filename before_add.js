9bfacc256224782caf8b14993d3857c8a4e5abab
Alexey <Aly8356@gmail.com>
2020-08-12 19:46:30 +0400

    before add

--- before_add.js
+++ before_add.js
@@ -4,11 +4,17 @@ const copydir = require('copy-dir');
 const config = require('./config');
 
 
-copydir(path.resolve(`${config.DEPLOY_SOURCE}`), path.resolve(`${config.DIST_DIR}`), {
-  utimes: true,  // keep add time and modify time
-  mode: true,    // keep file mode
-  cover: true    // cover file when exists, default is true
-}, function(err){
-  if(err) throw err;
-  console.log('done');
-});
+/**
+ * Copy deploy files into dest directory.
+ * @param {Object} git Git instance.
+ */
+exports.default = function(git) {
+    copydir(path.resolve(`${config.DEPLOY_SOURCE}`), path.resolve(`${config.DIST_DIR}`), {
+        utimes: true,  // keep add time and modify time
+        mode: true,    // keep file mode
+        cover: true    // cover file when exists, default is true
+    }, function(err){
+        if(err) throw err;
+        console.log('done');
+    });
+};
