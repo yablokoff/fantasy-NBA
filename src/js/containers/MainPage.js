29cb559863b63b31353681ca0b245429bb374ba2
Alexey <Aly8356@gmail.com>
2020-08-21 22:21:32 +0400

    save to storage in reducer

--- src/js/containers/MainPage.js
+++ src/js/containers/MainPage.js
@@ -9,18 +9,14 @@ import { CARDS_SHOW_COUNT } from "../constants/defaults";
 import { showPreloader, hidePreloader, loadDailyPlayers } from "../actions";
 import { LOAD_DAILY_PLAYERS_FULFILLED } from "../constants/ActionTypes";
 import { formattingDailyPlayersResponse } from "../api";
-import { fetchedCards } from "../storage/cards";
-import { getRandomIntList } from "../utils";
 import routes from "../constants/routes";
 
 
 const MainPage = () => {
     const dispatch = useDispatch();
-    const fetched_ids = fetchedCards.get();
-    const card_ids = fetched_ids && fetched_ids.card_ids || [];
-    // const { ids: fetched_ids } = useSelector(state => state.fetchedCardsIDs);
+    const { ids: fetched_ids } = useSelector(state => state.fetchedCardsIDs);
     const { ids: selected_ids } = useSelector(state => state.selectedCardsIDs);
-    const hasFetched = card_ids.length;
+    const hasFetched = fetched_ids.length;
     const hasSelected = selected_ids.length;
 
     useEffect(() => {
@@ -35,22 +31,10 @@ const MainPage = () => {
                         const count = formattedData.length;
 
                         if (count < CARDS_SHOW_COUNT) {
+                            // случай захода на страницу во время заполнения таблицы
                             dispatch(hidePreloader());
                             return Promise.resolve(null);
                         }
-
-                        const indexes = getRandomIntList({
-                            count: CARDS_SHOW_COUNT,
-                            rangeMin: 0,
-                            rangeMax: count - 1
-                        });
-
-                        const pd_ids = formattedData.map(pd => pd.id);
-                        const cards_ids = formattedData.map(pd => pd.card_id);
-                        fetchedCards.set({
-                            pd_ids: pd_ids.filter((id, i) => indexes.includes(i)),
-                            card_ids: cards_ids.filter((id, i) => indexes.includes(i)),
-                        });
                     }
                 }, (error) => {}
             );
@@ -64,7 +48,7 @@ const MainPage = () => {
             <Page>
                 <div className="cards-wrapper">
                     <div className="container">
-                        {hasFetched ? <MainList fetched_ids={card_ids} /> : <MainEmpty />}
+                        {hasFetched ? <MainList fetched_ids={fetched_ids} /> : <MainEmpty />}
                     </div>
                 </div>
             </Page>
--- src/js/reducers/fetchedCards.js
+++ src/js/reducers/fetchedCards.js
@@ -1,6 +1,8 @@
 import * as ActionTypes from "../constants/ActionTypes";
 import { formattingDailyPlayersResponse } from "../api";
+import { getRandomIntList } from "../utils";
 import { fetchedCards } from "../storage/cards";
+import { CARDS_SHOW_COUNT } from "../constants/defaults";
 
 
 const data = fetchedCards.getOrInvalidate();
@@ -28,10 +30,34 @@ export default (state = initialState, action) => {
             };
         }
         case ActionTypes.LOAD_DAILY_PLAYERS_FULFILLED:
+            // полное говно, но отсюда удобнее всего сохранить в стораж
             const formattedData = formattingDailyPlayersResponse(action.payload.data);
+
+            let filtered_cards_ids;
+            const count = formattedData.length;
+
+            if (count < CARDS_SHOW_COUNT) {
+                // случай захода на страницу во время заполнения таблицы
+                filtered_cards_ids = [];
+            } else {
+                const indexes = getRandomIntList({
+                    count: CARDS_SHOW_COUNT,
+                    rangeMin: 0,
+                    rangeMax: count - 1
+                });
+
+                const pd_ids = formattedData.map(pd => pd.id);
+                const cards_ids = formattedData.map(pd => pd.card_id);
+                filtered_cards_ids = cards_ids.filter((id, i) => indexes.includes(i));
+                fetchedCards.set({
+                    pd_ids: pd_ids.filter((id, i) => indexes.includes(i)),
+                    card_ids: filtered_cards_ids,
+                });
+            }
+
             return {
                 ...state,
-                ids: formattedData.map(pd => pd.card_id),
+                ids: filtered_cards_ids,
                 isFetching: false,
                 error: false
             };
