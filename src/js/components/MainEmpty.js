87b77b91d29f0d17e019cf66596ee82ec0edf875
Alexey <Aly8356@gmail.com>
2020-09-27 11:52:52 +0400

    empty text animation

--- src/js/components/MainEmpty.js
+++ src/js/components/MainEmpty.js
@@ -1,55 +1,31 @@
 import React from "react";
-import anime from "animejs";
-import SplitText from "./SplitText";
+import Anime from "@mollycule/react-anime";
 
-
-const firstPartText = "Hey! The contests are either live or unavailable today. Look out for updates on ";
-const secondPartText = "Frank's Instagram";
-const thirdPartText = ".";
 
 export default () => {
     return (
-        <div className="empty-label">
-            <SplitText
-                text={firstPartText}
-                duration={500}
-                onEnterAnimation={{
-                    opacity: [0, 1],
-                    translateX: function(el, index) {
-                        return [-1 * el.offsetLeft, 0];
-                    },
-                    delay: anime.stagger(40),
-                    easing: "easeOutQuint"
-                }} />
+        <Anime
+            in
+            duration={800}
+            appear
+            onEntering={{
+                opacity: [0, 1],
+                translateY: [220, 0],
+                easing: "easeInOutSine"
+            }}>
 
-            <a className="link"
-               target="_blank"
-               title="FRANK MICHAEL SMITH'S"
-               href="https://www.instagram.com/frankmichaelsmith_/">
-                <SplitText
-                    text={secondPartText}
-                    duration={500}
-                    onEnterAnimation={{
-                        opacity: [0, 1],
-                        translateX: function(el, index) {
-                            return [-1 * el.offsetLeft, 0];
-                        },
-                        delay: anime.stagger(40, {start: firstPartText.length * 40}),
-                        easing: "easeOutQuint"
-                    }} />
-            </a>
+            <div className="empty-label">
+                {"Hey! The contests are either live or unavailable today. Look out for updates on "}
 
-            <SplitText
-                text={thirdPartText}
-                duration={500}
-                onEnterAnimation={{
-                    opacity: [0, 1],
-                    translateX: function(el, index) {
-                        return [-1 * el.offsetLeft, 0];
-                    },
-                    delay: anime.stagger(40, {start: firstPartText.length * 40 + secondPartText.length * 40}),
-                    easing: "easeOutQuint"
-                }} />
-        </div>
+                <a className="link"
+                   target="_blank"
+                   title="FRANK MICHAEL SMITH'S"
+                   href="https://www.instagram.com/frankmichaelsmith_/">
+                    {"Frank's Instagram"}
+                </a>
+
+                {"."}
+            </div>
+        </Anime>
     )
 };
