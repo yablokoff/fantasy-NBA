d839a9c10429ae0f55d27395197b87ee9f563ec9
Alexey <Aly8356@gmail.com>
2023-07-22 17:04:49 +0400

    debug content request

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useGetContentQuery } from '../app/services/content';
 
 const Page = () => {
-    const { isLoading } = useGetContentQuery(undefined, {
+    const { isLoading, data } = useGetContentQuery(undefined, {
+        refetchOnReconnect: true,
         selectFromResult: ({ data }) => ({
             isLoading: !Boolean(data),
+            data,
         }),
     });
+    console.log('data:', data);
+    console.log('isLoading:', isLoading);
+
+    useEffect(() => {
+        return () => {
+            console.log('useEffect unmount:', data, isLoading);
+        };
+    }, []);
 
     return (
         <div id="app-page">
